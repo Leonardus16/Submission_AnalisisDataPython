0331a47734422879c41f22518e89f1d61fae2a56
Leonardus16 <154725236+Leonardus16@users.noreply.github.com>
2025-03-15 14:25:10 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -190,7 +190,7 @@ with tab2:
             st.markdown(f"""
                 <div class="metric-container">
                     <h4 style="margin:0; font-size:1rem; font-weight:500; color:#888">Penyewaan Jam Sibuk (11:00-16:00, Bukan Hari Kerja)</h4>
-                    <h2 style="margin:0; font-size:1.8rem; font-weight:600;">{non_working_rentals_recreation:} sepeda</h2>
+                    <h2 style="margin:0; font-size:1.8rem; font-weight:600;">{'non_working_rentals_recreation':} sepeda</h2>
                     <p style="margin:5px 0 0; font-size:0.8rem; color:#888;">Periode Rekreasi</p>
                 </div>
             """, unsafe_allow_html=True)
